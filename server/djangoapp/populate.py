d459294b3739aa2050f945de32ce3a7507a79a67
Priya-kumari31 <singhkhushbu8081@gmail.com>
2026-07-12 08:26:03 -0400

    Add car models and review functionality

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,6 +1,51 @@
-from djangoapp.models import Dealer
+from djangoapp.models import Dealer, CarMake, CarModel
+
 
 def initiate():
+
+    # Car Makes
+    car_makes = [
+        {
+            "name": "Toyota",
+            "description": "Toyota Cars"
+        },
+        {
+            "name": "Honda",
+            "description": "Honda Cars"
+        },
+        {
+            "name": "BMW",
+            "description": "BMW Cars"
+        },
+        {
+            "name": "Ford",
+            "description": "Ford Cars"
+        }
+    ]
+
+    for make in car_makes:
+        car_make, created = CarMake.objects.get_or_create(
+            name=make["name"],
+            description=make["description"]
+        )
+
+        models = {
+            "Toyota": ["Camry", "Corolla"],
+            "Honda": ["Civic", "Accord"],
+            "BMW": ["X5", "X3"],
+            "Ford": ["Mustang", "Explorer"]
+        }
+
+        for model in models[make["name"]]:
+            CarModel.objects.get_or_create(
+                car_make=car_make,
+                name=model,
+                type="Sedan",
+                year=2023
+            )
+
+
+    # Dealers
     dealer_data = [
         {
             "full_name": "ABC Motors",
@@ -15,18 +60,11 @@ def initiate():
             "address": "456 Market Street",
             "zip": "77001",
             "state": "Texas"
-        },
-        {
-            "full_name": "Prime Auto",
-            "city": "Austin",
-            "address": "789 Lake Road",
-            "zip": "73301",
-            "state": "Texas"
         }
     ]
 
     for data in dealer_data:
-        Dealer.objects.create(
+        Dealer.objects.get_or_create(
             full_name=data["full_name"],
             city=data["city"],
             address=data["address"],
